73a9ca8a8c5349868fbb44a1fa4b28504f5f50a1
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-10 14:24:59 -0300

    feat: initial Employe

---
+++ src/main/java/pablo/tzeliks/emploject/domain/model/Employe.java
@@ -0,0 +1,60 @@
+package pablo.tzeliks.emploject.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "employe")
+public class Employe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @ManyToMany
+    private List<Project> projects = new ArrayList<Project>();
+
+    public Employe(String nome, String phoneNumber, List<Project> projects) {
+        this.nome = nome;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+}
---
+++ src/main/java/pablo/tzeliks/emploject/domain/model/Project.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.emploject.domain.model;
+
+public class Project {
+}
