bacb3bdcef396e2f764631134d1417a531b2322e
PabloTzeliks <arq.pabloo@gmail.com>
2026-04-10 14:20:21 -0300

    Initial Commit

---
+++ src/main/java/pablo/tzeliks/emploject/EmplojectApplication.java
@@ -0,0 +1,13 @@
+package pablo.tzeliks.emploject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pablo/tzeliks/emploject/EmplojectApplicationTests.java
@@ -0,0 +1,13 @@
+package pablo.tzeliks.emploject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmplojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
